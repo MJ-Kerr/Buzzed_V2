7222a13a66eac963f6e2785f29f1c87404a81613
Miles Kerr <m.kerr.professional@gmail.com>
2023-01-24 09:17:06 -0600

    miles change

--- index.js
+++ index.js
@@ -8,38 +8,136 @@ const cocktailInfo = document.getElementById("info")
 const url = "http://localhost:3000/cocktails"
 
 
+fetch(url)
+    .then(response => response.json())
+//    .then((data) => (renderCocktail(data)))
+    .then((data) => console.log(data))
 
-function renderCocktail(cocktail){
-    const li = document.createElement("li")
-    li.className = "list-li";
 
-    const img = document.createElement("img");
-    img.src = cocktail.image_url;
-    img.alt = cocktail.name;
-    li.appendChild(img);
 
-    const h3 = document.createElement("h3");
-    h3.textContent = cocktail.name;
-    li.appendChild(h3);
+// Create a form element
+const form = document.createElement("form");
 
-    const p = document.createElement("p");
-    p.textContent = `Ingredients: ${cocktail.ingredients}`;
-    li.appendChild(p);
+// Create input elements
+const inputName = document.createElement("input");
+const inputImage = document.createElement("input");
+const inputIngredients = document.createElement("input");
+const inputProfile = document.createElement("input");
+const inputInfo = document.createElement("input");
+const inputPrep = document.createElement("input");
 
-    const button = document.createElement("button");
-    button.textContent = "Add to Cocktail List";
-    li.appendChild(button);
+// Add attributes to the input elements
+inputName.setAttribute("type", "text");
+inputName.setAttribute("placeholder", "Name");
+inputImage.setAttribute("type", "file");
+inputImage.setAttribute("placeholder", "Image");
+inputIngredients.setAttribute("type", "text");
+inputIngredients.setAttribute("placeholder", "Ingredients");
+inputProfile.setAttribute("type", "text");
+inputProfile.setAttribute("placeholder", "Profile");
+inputInfo.setAttribute("type", "text");
+inputInfo.setAttribute("placeholder", "Info");
+inputPrep.setAttribute("type", "text");
+inputPrep.setAttribute("placeholder", "Prep");
+
+// Add the input elements to the form
+form.appendChild(inputName);
+form.appendChild(inputImage);
+form.appendChild(inputIngredients);
+form.appendChild(inputProfile);
+form.appendChild(inputInfo);
+form.appendChild(inputPrep);
+
+// Create a submit button
+const submitButton = document.createElement("input");
+submitButton.setAttribute("type", "submit");
+submitButton.setAttribute("value", "Submit");
+
+// Add the submit button to the form
+form.appendChild(submitButton);
+
+// Add an event listener to the form to handle the submit event
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  // Get the values of the input elements
+  const name = inputName.value;
+  const image = inputImage.value;
+  const ingredients = inputIngredients.value;
+  const profile = inputProfile.value;
+  const info = inputInfo.value;
+  const prep = inputPrep.value;
+  console.log("Name: " + name);
+  console.log("Image: " + image);
+  console.log("Ingredients: " + ingredients);
+  console.log("Profile: " + profile);
+  console.log("Info: " + info);
+  console.log("Prep: " + prep);
+});
+
+// Append the form to the body of the page
+document.body.appendChild(form);
+    //function renderCocktail(cocktail){
+//        const li = document.createElement("li")
+//        li.className = "list-li";
+//
+//        const img = document.createElement("img");
+//        img.src = cocktail.image_url;
+//        img.alt = cocktail.name;
+//        li.appendChild(img);
+//
+//        const h3 = document.createElement("h3");
+//        h3.textContent = cocktail.name;
+//        li.appendChild(h3);
+//
+//        const p = document.createElement("p");
+//        p.textContent = `Ingredients: ${cocktail.ingredients}`;
+//        li.appendChild(p);
+//
+//        const button = document.createElement("button");
+//        button.textContent = "Add to Cocktail List";
+//        li.appendChild(button);
+//
+        //cocktailInfo.appendChild(li);
+
+   // const button = document.createElement("button");
+    //button.textContent = "Add to Cocktail List";
+    //li.appendChild(button);
 
     
+//};
+
 
     
-};
+//};
 
-
-
-
-
-
+//// Get the button by its ID
+//const button = document.getElementById("randomize-button");
+//
+//// Add an event listener to the button to trigger the randomizer function on click
+//button.addEventListener("click", getRandomItem);
+//
+//function getRandomItem() {
+//  // Make a GET request to the local API
+//  fetch('url')
+//    .then(response => response.json())
+//    .then((data) => console.log(data))
+//    .then(data => {
+//      // Get the length of the array
+//      let length = data.length;
+//      // Generate a random index
+//      let randomIndex = Math.floor(Math.random() * length);
+//      // Get the random item from the array
+//      let randomItem = data[randomIndex];
+//      // Do something with the random item
+//      console.log(randomItem);
+//    })
+//    .catch(error => console.log(error));
+//}
+//
+//
+//
+//
+//
 
 
 
